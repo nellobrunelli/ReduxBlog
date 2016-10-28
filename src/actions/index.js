f11ee5b43afb1298c69711fb895077859b699464
Nello Brunelli <nello.brunelli@wyscout.com>
2016-10-28 17:43:50 +0200

    Reducer for select post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,23 @@
 import axios from 'axios';
+import * as type from '../constant/ActionTypes';
+
 export const FETCH_POSTS = 'http://localhost:3000/posts';
 
 export function fetchPosts() {
     const request = axios.get(FETCH_POSTS);
     return (dispatch) => {
         request.then((data) => {
-            dispatch({type: 'FETCH_POSTS', payload: data});
+            dispatch({
+                type: 'FETCH_POSTS',
+                payload: data
+            });
         });
     };
 }
 
-
-// export function selectPost() {
-//     const request = axios.post(FETCH_POSTS);
-//     return {
-//         type: 'SELECT',
-//         payload: request
-//     };
-// }
+export function selectPost(post) {
+    return {
+        type: type.SELECT_POST,
+        payload: post
+    };
+}
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,7 @@
 import * as type from '../constant/ActionTypes';
 const STATE_DEFAULT = {
     all: [],
-    selectedPost: [],
+    selectedPosts: [],
     post: null
 };
 
@@ -9,6 +9,9 @@ export default function(state = STATE_DEFAULT, action) {
     switch (action.type) {
         case type.FETCH_POSTS:
             return {...state, all: action.payload.data};
+        case type.SELECT_POST:
+            // @TODO: make it dynamic
+            return {...state, selectedPosts: [1]};
         default:
             return state;
     }
